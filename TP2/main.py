b8fed30f5730ed3683b2ca8e5564a677cc5b1199
Bast-94 <bh.69@live.fr>
2024-01-16 13:26:34 +0100

    ADD id to tracking

--- TP2/main.py
+++ TP2/main.py
@@ -1,4 +1,4 @@
-DATA_DIR = 'ADL-Rundle-6'
+DATA_DIR = "ADL-Rundle-6"
 import os
 import pandas as pd
 import numpy as np
@@ -10,62 +10,105 @@ import argparse
 import imageio
 import itertools
 from src.iou import BoundingBox, intersection_box, iou
-det_file = 'ADL-Rundle-6/det/det.txt' if os.path.exists('ADL-Rundle-6/det/clean_det.csv') else 'ADL-Rundle-6/det/det.txt'
 
-if not os.path.exists('ADL-Rundle-6/det/clean_det.csv'):
-    det_df = pd.read_csv(det_file, sep=',', header=None)
-    det_df.columns = ['frame', 'id', 'bb_left', 'bb_top', 'bb_width', 'bb_height', 'conf', 'x', 'y', 'z']
-    det_df.to_csv('ADL-Rundle-6/det/clean_det.csv', index=False)
+det_file = (
+    "ADL-Rundle-6/det/det.txt"
+    if os.path.exists("ADL-Rundle-6/det/clean_det.csv")
+    else "ADL-Rundle-6/det/det.txt"
+)
+
+if not os.path.exists("ADL-Rundle-6/det/clean_det.csv"):
+    det_df = pd.read_csv(det_file, sep=",", header=None)
+    det_df.columns = [
+        "frame",
+        "id",
+        "bb_left",
+        "bb_top",
+        "bb_width",
+        "bb_height",
+        "conf",
+        "x",
+        "y",
+        "z",
+    ]
+    det_df.to_csv("ADL-Rundle-6/det/clean_det.csv", index=False)
 else:
-    det_df = pd.read_csv('ADL-Rundle-6/det/clean_det.csv', sep=',', header=0)
+    det_df = pd.read_csv("ADL-Rundle-6/det/clean_det.csv", sep=",", header=0)
 
-BOUNDING_BOX_DIR = 'ADL-Rundle-6/bounding_boxes'
-IMG_DIR = 'ADL-Rundle-6/img1'
+BOUNDING_BOX_DIR = "ADL-Rundle-6/bounding_boxes"
+IMG_DIR = "ADL-Rundle-6/img1"
 img_file_list = sorted(os.listdir(IMG_DIR))
 
 arg_parser = argparse.ArgumentParser()
-arg_parser.add_argument('-n', '--n-frame', type=int, default=6, help='Frame number')
-arg_parser.add_argument('-g', '--gif', action='store_true', help='Create gif')
+arg_parser.add_argument("-n", "--n-frame", type=int, default=1, help="Frame number")
+arg_parser.add_argument("-g", "--gif", action="store_true", help="Create gif")
 
 args = arg_parser.parse_args()
 nb_frame = args.n_frame
 save_gif = args.gif
 img_file_list = img_file_list[:nb_frame]
 
+threshold = 0.5
+cur_id = 0
 for n_frame, img_file in tqdm(enumerate(img_file_list, start=1)):
-    img_file = img_file_list[n_frame-1]
+    img_index = n_frame - 1
+    img_file = img_file_list[img_index]
     img = Image.open(os.path.join(IMG_DIR, img_file))
-    frame_data = det_df[det_df['frame']==n_frame]
+
+    frame_data = det_df[det_df["frame"] == n_frame]
+    next_frame_data = det_df[det_df["frame"] == n_frame + 1]
     opencv_img = cv2.imread(os.path.join(IMG_DIR, img_file))
-    row_cominations = list(itertools.combinations(frame_data.index, 2))
-    iou_matrix = np.zeros((len(row_cominations), len(row_cominations)))
-    for row1, row2 in row_cominations:
-        bb1 = BoundingBox(frame_data.loc[row1, 'bb_left'], frame_data.loc[row1, 'bb_top'], frame_data.loc[row1, 'bb_width'], frame_data.loc[row1, 'bb_height'])
-        bb2 = BoundingBox(frame_data.loc[row2, 'bb_left'], frame_data.loc[row2, 'bb_top'], frame_data.loc[row2, 'bb_width'], frame_data.loc[row2, 'bb_height'])
-
+    for i, row1 in enumerate(frame_data.index):
         
-
-    for row in (frame_data.iloc):
-        bb_left = int(row['bb_left'])
-        bb_top = int(row['bb_top'])
-        bb_width = int(row['bb_width'])
-        bb_height = int(row['bb_height'])
-        if save_gif:
-            cv2.rectangle(opencv_img, (bb_left, bb_top), (bb_left+bb_width, bb_top+bb_height), (0,0,255), 2)
-            cv2.putText(opencv_img, str(row['id']), (bb_left, bb_top), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
-            cv2.imwrite(os.path.join(BOUNDING_BOX_DIR, img_file), opencv_img)
-        
-
+        for j, row2 in enumerate(next_frame_data.index):
+            
+            bb1 = BoundingBox(
+                frame_data["bb_left"][row1],
+                frame_data["bb_top"][row1],
+                frame_data["bb_width"][row1],
+                frame_data["bb_height"][row1],
+            )
+            bb2 = BoundingBox(
+                next_frame_data["bb_left"][row2],
+                next_frame_data["bb_top"][row2],
+                next_frame_data["bb_width"][row2],
+                next_frame_data["bb_height"][row2],
+            )
+            iou_score = iou(bb1, bb2)
+            if det_df.loc[row1, "id"] == -1:
+                det_df.loc[row1, "id"] = cur_id
+                cur_id += 1
+            if iou_score >= threshold:
+                det_df.loc[row2, "id"] = det_df.loc[row1, "id"]
+            if save_gif:
+                cv2.rectangle(
+                    opencv_img,
+                    (int(bb1.bb_left), int(bb1.bb_top)),
+                    (int(bb1.bb_left + bb1.bb_width ), int(bb1.bb_top + bb1.bb_height)),
+                    (0, 0, 255),
+                    2,
+                )
+                cv2.putText(
+                    opencv_img,
+                    str(det_df.loc[row1, "id"]),
+                    (int(bb1.bb_left), int(bb1.bb_top)),
+                    cv2.FONT_HERSHEY_SIMPLEX,
+                    1,
+                    (0, 0, 255),
+                    2,
+                    cv2.LINE_AA,
+                )
+                cv2.imwrite(
+                    os.path.join(BOUNDING_BOX_DIR, img_file),
+                    opencv_img,
+                )
+    
+    det_df.to_csv("ADL-Rundle-6/det/restults.csv", index=False)
+    
 
 if save_gif:
-    
-    print('Creating gif...')
+    print("Creating gif...")
     images = []
-    for filename in sorted(os.listdir(BOUNDING_BOX_DIR))[:10]:
+    for filename in sorted(os.listdir(BOUNDING_BOX_DIR))[:nb_frame]:
         images.append(imageio.imread(os.path.join(BOUNDING_BOX_DIR, filename)))
-    imageio.mimsave('ADL-Rundle-6/bounding_boxes.gif', images, duration=0.5)
-        
-        
-    
-
-    
+    imageio.mimsave("ADL-Rundle-6/bounding_boxes.gif", images, duration=0.5)
--- TP2/src/iou.py
+++ TP2/src/iou.py
@@ -2,22 +2,28 @@ from collections import namedtuple
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.patches import Rectangle
-BoundingBox = namedtuple('BoundingBox', ('bb_left', 'bb_top', 'bb_width', 'bb_height'))
+
+BoundingBox = namedtuple("BoundingBox", ("bb_left", "bb_top", "bb_width", "bb_height"))
+
 
 def print_bounding_box(bb):
-    print(f'bb_left: {bb.bb_left}, bb_top: {bb.bb_top}, bb_width: {bb.bb_width}, bb_height: {bb.bb_height}')
+    print(
+        f"bb_left: {bb.bb_left}, bb_top: {bb.bb_top}, bb_width: {bb.bb_width}, bb_height: {bb.bb_height}"
+    )
+
 
 def intersection_box(bb1, bb2):
     bb_left = max(bb1.bb_left, bb2.bb_left)
     bb_top = max(bb1.bb_top, bb2.bb_top)
-    bb_right = min(bb1.bb_left+bb1.bb_width, bb2.bb_left+bb2.bb_width)
-    bb_bottom = min(bb1.bb_top+bb1.bb_height, bb2.bb_top+bb2.bb_height)
+    bb_right = min(bb1.bb_left + bb1.bb_width, bb2.bb_left + bb2.bb_width)
+    bb_bottom = min(bb1.bb_top + bb1.bb_height, bb2.bb_top + bb2.bb_height)
     height = bb_bottom - bb_top
     width = bb_right - bb_left
     if height < 0 or width < 0:
         return BoundingBox(bb_left, bb_top, 0, 0)
 
-    return BoundingBox(bb_left, bb_top, bb_right-bb_left, bb_bottom-bb_top)
+    return BoundingBox(bb_left, bb_top, bb_right - bb_left, bb_bottom - bb_top)
+
 
 def iou(bb1, bb2):
     intersection = intersection_box(bb1, bb2)
@@ -27,32 +33,57 @@ def iou(bb1, bb2):
     union_area = bb1_area + bb2_area - intersection_area
     return intersection_area / union_area
 
+
 if __name__ == "__main__":
     fig, ax = plt.subplots()
 
-    #create simple line plot
-    
+    # create simple line plot
 
-    #add rectangle to plot
-    
-    fig,ax = plt.subplots()
+    # add rectangle to plot
+
+    fig, ax = plt.subplots()
     ax.set_xlim([0, 30])
     ax.set_ylim([0, 30])
-    
+
     bb1 = BoundingBox(0, 0, 10, 10)
-    print('bb1:')
+    print("bb1:")
     print_bounding_box(bb1)
-    ax.add_patch(Rectangle((bb1.bb_left,bb1.bb_top), bb1.bb_width, bb1.bb_height, fill=False, edgecolor='red', linewidth=2))
+    ax.add_patch(
+        Rectangle(
+            (bb1.bb_left, bb1.bb_top),
+            bb1.bb_width,
+            bb1.bb_height,
+            fill=False,
+            edgecolor="red",
+            linewidth=2,
+        )
+    )
     bb2 = BoundingBox(5, 5, 10, 10)
-    print('bb2:')
+    print("bb2:")
     print_bounding_box(bb2)
-    ax.add_patch(Rectangle((bb2.bb_left,bb2.bb_top), bb2.bb_width, bb2.bb_height, fill=False, edgecolor='blue', linewidth=2))
+    ax.add_patch(
+        Rectangle(
+            (bb2.bb_left, bb2.bb_top),
+            bb2.bb_width,
+            bb2.bb_height,
+            fill=False,
+            edgecolor="blue",
+            linewidth=2,
+        )
+    )
     bb3 = intersection_box(bb1, bb2)
-    ax.add_patch(Rectangle((bb3.bb_left, bb3.bb_top), bb3.bb_width, bb3.bb_height, fill=True,  linewidth=2, color='green'))
-    print('bb3:')
+    ax.add_patch(
+        Rectangle(
+            (bb3.bb_left, bb3.bb_top),
+            bb3.bb_width,
+            bb3.bb_height,
+            fill=True,
+            linewidth=2,
+            color="green",
+        )
+    )
+    print("bb3:")
     print_bounding_box(bb3)
-    print('iou(bb1, bb2):', iou(bb1, bb2))
-    
-    fig.savefig('iou.png')
+    print("iou(bb1, bb2):", iou(bb1, bb2))
 
-
+    fig.savefig("iou.png")
