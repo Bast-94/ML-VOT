216e8597de651f8c8985bea04ba0f79fc9bda3bf
Bast-94 <bh.69@live.fr>
2024-01-26 21:11:17 +0100

    ADD(get_artifacts_parsers).

--- src/parsers.py
+++ src/parsers.py
@@ -8,4 +8,8 @@ def get_track_args():
     arg_parser.add_argument("-H","--hungarian", action="store_true", help="Use Hungarian algorithm")
     return arg_parser.parse_args()
 
-
+def get_artifacts_args():
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument("-c", "--config", type=str, default="secretdir/config.yml", help="Config file")
+    
+    return arg_parser.parse_args()
