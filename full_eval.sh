dea4ba17661c68d93043f1ce6805a3b67dad5ab1
Bast-94 <bh.69@live.fr>
2024-01-28 21:54:17 +0100

    FIx:track

--- full_eval.sh
+++ full_eval.sh
@@ -6,6 +6,7 @@ cp produced/$RESULT_FILE $MOT_DIR/$mode_name/data/ADL-Rundle-6.txt
 cd TrackEval/
 
 python scripts/run_mot_challenge.py --BENCHMARK MOT15 --SPLIT_TO_EVAL train --TRACKERS_TO_EVAL $mode_name --METRICS HOTA CLEAR Identity --USE_PARALLEL False  --NUM_PARALLEL_CORES 1
+cd ..
 mkdir -p produced/$mode_name
 
 cp $MOT_DIR/$mode_name/pedestrian_plot.png produced/$mode_name/.
